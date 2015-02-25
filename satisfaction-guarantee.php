73888703014cb7c6a919197b3a63f0a4a2965498
Leo Fernandez <leox.fernandez@gmail.com>
2015-02-25 17:48:45 +0800

    minor changes

---
+++ satisfaction-guarantee.php
@@ -0,0 +1,13 @@
+
+<p class="iJustify"><strong>The Plantation Bay Room and Restaurant Satisfaction Guarantee.</strong>  We would like all our guests to feel they made the right choice in coming here. And most of our guests do, so we can afford to be liberal with those who don’t:</p>
+				<p class="iJustify"><strong><em>Immediate change of heart after checking in. </em> </strong>If after arrival you do not think you would enjoy your stay, please advise us (by calling a Duty Manager or the Front Office Manager) within 4 hours of checking in, and we will do the following:</p>
+				<ol class="iJustify">
+					<li>waive your room charge for the day (but not your charges for food, goods, or services if any);</li>
+					<li>in case you made any advance payment or deposit, even for peak or blackout periods,<br />
+						the entire amount will be refunded to you in cash in Philippine peso equivalent;</li>
+					<li>help you book at another hotel in metropolitan Cebu; and</li>
+					<li>provide you with free chauffeur-driven transportation to that hotel;</li>
+				</ol>
+				<p class="iJustify">You must, however, actually come. Our normal policies apply to no-shows and reservation cancellations made prior to check-in.</p>
+				<p class="iJustify"><em><strong>Early departure vis-à-vis the number of nights reserved. </strong> </em>If after staying a day or more during a non-peak and non-blackout period, you decide to cut short your reserved stay, you will only be charged for the days actually stayed (you must of course leave by 12 Noon, our standard check-out time). In case you made any advance payment or deposit, the unused balance will be refunded to you in Philippine peso equivalent. Please note however that if you were accorded a discount due to the length of stay booked, an early departure may disqualify you from the discount, and the rate for the days you actually stayed will be calculated accordingly. This section does not apply to peak dates or dates that are “blacked out”, i.e., dates for which advance payment in full is required (roughly 30 days a year are peak or blackout dates); in respect of such dates, no refunds will be given for early departures.</p>
+				<p class="iJustify"><em><strong>Unsatisfactory food.  </strong></em>When dining or drinking at any of our restaurants (but not our dinner shows or open buffets), if after eating or drinking 25% or less of the serving, any dish or drink is not to your absolute satisfaction, please advise our staff and there will be no charge for it. (We will of course offer you alternatives.) Please do this instead of just grousing about your meal later, when we can no longer ascertain exactly what the problem was or whether any staff member warrants correction.</p>
